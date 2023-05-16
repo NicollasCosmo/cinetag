444fe22d4fdb32735d2727eba349b07bd091fee2
Nicollas Cosmo <nicollascosmo@hotmail.com>
2023-05-16 16:47:13 -0300

    last update

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,17 @@
+import styles from './Rodape.module.css';
+
+
+function Rodape() {
+    return(
+        <footer className={styles.rodape}>
+
+            <h2>Desenvolvido por Cosmo</h2>
+        </footer>
+      
+    )
+        
+       
+}
+
+
+export default Rodape;
