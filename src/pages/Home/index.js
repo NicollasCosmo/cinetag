6c006eed347adfb22d09799aaa344d8725abe313
Nicollas Cosmo <nicollascosmo@hotmail.com>
2023-05-16 16:45:27 -0300

    last update

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import Cabecalho from "../../components/Cabecalho";
+import Rodape from "../../components/Rodape";
+
+function Home() {
+    return(
+
+
+        <>
+        <Cabecalho />
+        <Rodape />
+        </>
+        
+    )
+}
+
+export default Home;
